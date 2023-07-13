6d97b1d94be04fe3c024658f323ce6b1e630408e
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2023-07-14 03:57:38 +0800

    ShooterGame Prod 1.0
    Added shooting mechanics
    Touhou Fan Made-ish
    Bullet destruction after certain time elapses
    Bullet rate of fire adjustable

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,64 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public float moveSpeed;
+    private float defaultMoveSpeed = 4;
     public Rigidbody2D rigidBody;
     private Vector2 moveInput;
     public Animator anim;
-    private SpriteRenderer spriteRenderer;
-
-    public GameObject bulletPrefab;
+    //private SpriteRenderer spriteRenderer;
     public Transform bulletSpawnPoint;
     public float bulletSpeed;
+    
     public float fireRate;
-    float firecooldown;
 
+    private GameObject bullet;
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    private bool canShoot = false;
+
+    public float timeToLive = 5f;
     // Start is called before the first frame update
     void Start()
     {
-
         rigidBody= GetComponent<Rigidbody2D>();
         anim= GetComponent<Animator>();
-        spriteRenderer= GetComponent<SpriteRenderer>();
-        if(spriteRenderer == null)
-        {
-            Debug.Log("Sprite missing a renderer");
-        }
-
-        firecooldown = fireRate;
-        fireRate= 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(Input.GetAxisRaw("Horizontal") > 0)
-        {
-            spriteRenderer.flipX = false;
-            //Debug.Log("Sprite flip false");
-        }
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            spriteRenderer.flipX = true;
-           // Debug.Log("Sprite flip true");
-        }
+        
         anim.SetFloat("Horizontal", moveInput.x);
         anim.SetFloat("Vertical", moveInput.y);
         anim.SetFloat("Speed", moveInput.sqrMagnitude);
         //Debug.Log("Test");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
-            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-            rb.velocity = transform.up * bulletSpeed;
+            Debug.Log("Reduce speed");
+            moveSpeed -= 2;
         }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (moveSpeed < defaultMoveSpeed)
+            {
+                Debug.Log("Return default move speed");
+                moveSpeed = defaultMoveSpeed;
+            }
+        }
+        
+
+        if (Input.GetButton("Fire1") &&(!canShoot))
+        {   
+            StartCoroutine(FireShot());        
+        }
+
+        Destroy(bullet, timeToLive);
+       
     }
+
+
+    IEnumerator FireShot()
+    {
+        canShoot = true;
+        Fire();
+        yield return new WaitForSeconds(fireRate);
+        canShoot = false;
+    }
+
+
+
+    void Fire()
+    {
+        bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * bulletSpeed;
+    }
+
+    
 
     private void FixedUpdate()
     {
